081d8e7173e2b3fadd285b449ced9153604adce7
Peter Wang <happypeter1983@gmail.com>
2018-01-20 11:50:37 +0800

    getter

--- src/components/CommentBox.vue
+++ src/components/CommentBox.vue
@@ -19,9 +19,7 @@
     computed: {
       postId() { return this.$route.params.id },
       comments() { 
-        return this.$store.state.comment.all.filter(
-          t => t.post === this.postId
-        )
+        return this.$store.getters.getComments(this.postId)
       },
       reversedComments() { return this.comments.slice().reverse() }
     },
